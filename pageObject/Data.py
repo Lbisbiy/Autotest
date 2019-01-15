9e8c94dfda21032bf68b40cf3909f00fde787b52
Lbisbiy <lbisbiy_andrey@mail.ru>
2019-01-16 00:54:46 +0400

    Update Data.py

--- pageObject/Data.py
+++ pageObject/Data.py
@@ -2,7 +2,7 @@
 class Data:
 
     def __init__(self):
-        self.myLogin = 'mischenko1andrey@gmail.com'
-        self.myPassword = '684481F630827f'
-        self.checkAddress = 'ilya.filinin@simbirsoft.com'
-        self.topicForLetter = 'Тестовое задание. Мищенко'
+        self.myLogin = 'AAAAAAAAAAAAAAAA.com'
+        self.myPassword = '1234567890'
+        self.checkAddress = 'BBBBBBBBBBBB.com'
+        self.topicForLetter = 'Тестовое задание.'
